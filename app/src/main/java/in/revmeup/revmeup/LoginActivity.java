b245fb3bbe3cc292b2093aa71a8487eb3d655665
purvi <51371107+purvi-n@users.noreply.github.com>
2020-06-23 21:17:17 +0530

    second

--- app/src/main/java/in/revmeup/revmeup/LoginActivity.java
+++ app/src/main/java/in/revmeup/revmeup/LoginActivity.java
@@ -19,7 +19,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+//test2
 public class LoginActivity extends AppCompatActivity {
 
     FirebaseAuth mFirebaseAuth=FirebaseAuth.getInstance();
