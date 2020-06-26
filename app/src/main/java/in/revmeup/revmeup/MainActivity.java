43b0532da0f1059a8289c8fce65f62fbe9463394
Rajalakshmi <rjl.subha@gmail.com>
2020-06-26 16:17:44 +0530

    splash

--- app/src/main/java/in/revmeup/revmeup/MainActivity.java
+++ app/src/main/java/in/revmeup/revmeup/MainActivity.java
@@ -33,20 +33,9 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //This method is used so that your splash activity
-        //can cover the entire screen.*/
+
         setContentView(R.layout.activity_main);
-        /* new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i=new Intent(MainActivity.this,
-                        SecondActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }, SPLASH_SCREEN_TIME_OUT);*/
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
---
+++ app/src/main/java/in/revmeup/revmeup/Splash.java
@@ -0,0 +1,39 @@
+package in.revmeup.revmeup;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.os.Handler;
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
